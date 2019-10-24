28902bd95dae913a3683ab4a350e330be721c240
Alexander Shuvalov <ashuvduddits@gmail.com>
2019-10-24 19:57:30 +0300

    update9

--- Test.php
+++ Test.php
@@ -1,8 +1,6 @@
 <?php
-require_once 'PHPUnit/Framework.php';
 
-
-class MyClassTest extends PHPUnit_Framework_TestCase {
+class MyClassTest extends \PHPUnit_Framework_TestCase {
   public function testPower()
   {
       $this->assertEquals(8, 8);
